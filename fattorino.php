452e62f657da566541c4e424cd722a5bea3103fb
Marco Pazzaglia <marco.pazzaglia4@studio.unibo.it>
2019-01-24 12:08:20 +0100

    fattorino quasi finito

--- fattorino.php
+++ fattorino.php
@@ -13,24 +13,60 @@ session_start();
 </head>
 <body>
   <?php
+
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
   include("php/config.php");
   $conn =new mysqli($servername, $username, $password, $db);
   $stato = NULL;
   $id = NULL;
 
-  $dbErr = "";
+  $dbErr1 = "";
+  $dbErr2 = "";
   if ($conn->connect_errno) {
   } else{
-    $query_sql="SELECT * FROM `account` WHERE email = 'fattorino@gmail.com'";
+    $query_sql="SELECT * FROM `account` WHERE email = '".$_SESSION["email"]."'";
     $result = $conn->query($query_sql);
     $row = $result->fetch_assoc();
   if ($result->num_rows > 0) {
     $stato = $row["disponibilita"];
     $id = $row["idAccount"];
+
+    if (!empty($_POST["email1"]) && !empty($_POST["email2"])){
+      $email1 = test_input($_POST["email1"]);
+      $email2 = test_input($_POST["email2"]);
+      if (strcmp($email1,$email2) == 0){
+        $sql = "UPDATE `account` SET `email`= '".$email1."' WHERE `idAccount` = '".$id."'";
+        $result = $conn->query($sql);
+      } else {
+        $dbErr1 = "I due indirizzi email non corrispondono!";
+      }
+    }
+
+    if (!empty($_POST["password1"]) && !empty($_POST["password2"])){
+      $password1 = test_input($_POST["password1"]);
+      $password2 = test_input($_POST["password2"]);
+      if (strcmp($password1,$password2) == 0){
+        $sql = "UPDATE `account` SET `password`= '".$password1."' WHERE `idAccount` = '".$id."'";
+        $result = $conn->query($sql);
+      } else {
+        $dbErr2 = "Le due password non corrispondono!";
+      }
+    }
+    if(isset($_POST['delete'])){
+      $sql = "DELETE FROM account WHERE idAccount = '".$id."'";
+      $result = $conn->query($sql);
+      header("location: index.php");
+    }
   ?>
-  <!--Header-->
+  <!--header-->
   <header class="header clearfix">
-    <a href="index.php" class="header__logo">
+    <a href="#" class="header__logo">
     <img src="image/logo-header.png" alt="logo" width="50px" height="50px">
     </a>
     <a href="" class="header__icon-bar">
@@ -39,8 +75,7 @@ session_start();
       <span></span>
     </a>
     <ul class="header__menu animate">
-      <li class="header__menu__item"><a href="index.php">Logout</a></li>
-      <li class="header__menu__item"><a href="contattaci.html">Contattaci</a></li>
+      <li class="header__menu__item"><a href="logout.php">Logout</a></li>
     </ul>
   </header>
 
@@ -66,6 +101,13 @@ session_start();
       <div class="col-lg-7 col-md-12 col-sm-12 col-help" id="tabella">
         <div class="table-responsive">
           <h3>Storico ordini</h3>
+          <?php
+
+          $sql = "SELECT * FROM `consegna` WHERE idAccount = ".$id;
+          $result = $conn->query($sql);
+          if ($result->num_rows > 0){
+            while($row = $result->fetch_assoc()) {
+              ?>
           <table class="table table-hover table-bordered">
             <thead class="thead-dark">
             <tr>
@@ -75,32 +117,34 @@ session_start();
               <th scope="col">Aula</th>
             </tr>
             </thead>
-            <tbody>	<?php
-
-            $sql = "SELECT * FROM `consegna` WHERE idAccount = '".$id."'";
-            $result = $conn->query($sql);
-        			while($row = $result->fetch_assoc()) {
-                $query_sql = "SELECT idRistorante FROM `ordine` WHERE idRistorante = '".$row["idOrdine"]."'";
+            <tbody>
+            <?php
+                $query_sql = "SELECT idRistorante FROM `ordine` WHERE idRistorante = ".$row["idOrdine"];
                 $res = $conn->query($query_sql);
-              if ($res->num_rows > 0) {
-                $idRist = $res->fetch_assoc();
-                $query_sql = "SELECT nome FROM `ristorante` WHERE idRistorante = '".$idRist["idRistorante"]."'";
-                $ris = $conn->query($query_sql);
-              if ($ris->num_rows > 0) {
-                $nome = $ris->fetch_assoc();
+                if ($res->num_rows > 0) {
+                  $idRist = $res->fetch_assoc();
+                  $query_sql = "SELECT nome FROM `ristorante` WHERE idRistorante = ".$idRist["idRistorante"];
+                  $ris = $conn->query($query_sql);
+                  if ($ris->num_rows > 0) {
+                    $nome = $ris->fetch_assoc();
         						?>
         						<tr>
-        							<td><?php echo $row["idAccount"]; ?></td>
+        							<td><?php echo $row["data"]; ?></td>
         							<td><?php echo $row["orario"]; ?></td>
         							<td><?php echo $nome["nome"]; ?></td>
                       <td><?php echo $row["aula"]; ?></td>
         						</tr>
         						<?php
-                    }
                   }
                 }
               }
-        			?>
+            } else {
+                    ?>
+                    <p>Nessuna consegna effettuata</p>
+                    <?php
+              }
+            }
+      		?>
             </tbody>
           </table>
         </div>
@@ -110,7 +154,7 @@ session_start();
         <div class="buttons">
           <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" id="button" data-target="#emailForm">Cambia email</button>
           <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" id="button" data-target="#passwordForm">Cambia password</button>
-          <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" id="button">Elimina account</button></span>
+          <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" id="button" data-target="#deleteForm">Elimina account</button></span>
       </div>
     </div>
   </div>
@@ -133,7 +177,7 @@ session_start();
               <label for="pwd">Conferma email:</label>
               <input type="email" name="email2" class="form-control" required>
             </div>
-            <span class="error"><?php echo $dbErr;?></span>
+            <span class="error"><?php echo $dbErr1;?></span>
             <div class="modal-footer">
                 <button type="submit" class="btn btn-primary mr-auto submit">Conferma</button>
                 <button type="button" class="btn btn-danger" data-dismiss="modal">Chiudi</button>
@@ -156,15 +200,38 @@ session_start();
           <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             <div class="form-group">
               <label for="email">Nuova password:</label>
-              <input type="password" name="password2" class="form-control" required>
+              <input type="password" name="password1" class="form-control" required>
             </div>
             <div class="form-group">
               <label for="pwd">Conferma password:</label>
               <input type="password" name="password2" class="form-control" required>
             </div>
-            <span class="error"><?php echo $dbErr;?></span>
+            <span class="error"><?php echo $dbErr2;?></span>
             <div class="modal-footer">
                 <button type="submit" class="btn btn-primary mr-auto submit">Conferma</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Chiudi</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Modal-->
+  <div class="modal" id="deleteForm">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Elimina account</h4>
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body">
+          <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <div class="form-group">
+              <label>Sei sicuro di voler eliminare l'account?</label>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" name="delete" class="btn btn-primary mr-auto submit">Conferma</button>
                 <button type="button" class="btn btn-danger" data-dismiss="modal">Chiudi</button>
             </div>
           </form>
@@ -199,16 +266,28 @@ session_start();
     $(document).ready(function(){
 
       <?php
-      if($dbErr == "I due indirizzi email non corrispondono!"){
+      if($dbErr1 != ""){
       ?>
-        $('#loginForm').modal('show');
+        $('#emailForm').modal('show');
       <?php
-      }
+    } else {
+      $email1 = "";
+      $email2 = "";
+    }
+      ?>
+
+      <?php
+      if($dbErr2 != ""){
+      ?>
+        $('#passwordForm').modal('show');
+      <?php
+    } else {
+      $password1 = "";
+      $password2 = "";
+    }
       ?>
 
     });
   </script>
-
-
   </body>
   </html>
