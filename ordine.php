172f052c11618232d96ebdaabaf952d65fcfb988
Elia <elia.pasqualini@studio.unibo.it>
2019-01-29 15:33:03 +0100

    ordine.php created

---
+++ ordine.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.css">
+  <link rel="stylesheet" type="text/css" href="css/cliente.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  <title>UniFood</title>
+</head>
+
+  <body>
+    <?php
+    include("php/config.php");
+    $conn =new mysqli($servername, $username, $password, $db);
+
+    ?>
+    <!--Header-->
+    <header class="header clearfix">
+      <a href="#" class="header__logo">
+      <img src="image/logo-header.png" alt="logo" width="50px" height="50px">
+      </a>
+      <a href="" class="header__icon-bar">
+        <span></span>
+        <span></span>
+        <span></span>
+      </a>
+      <ul class="header__menu animate">
+        <li class="header__menu__item">
+          <div class="dropdown">
+            <button class="dropbtn">Account
+              <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+              <a href="logout.php">Logout</a>
+              <a href="" data-toggle="modal" data-target="#emailForm">Modifica email</a>
+              <a href="" data-toggle="modal" data-target="#passwordForm">Modifica password</a>
+              <a href="" data-toggle="modal" data-target="#deleteForm">Elimina Account</a>
+            </div>
+          </div>
+        </li>
+        <li class="header__menu__item"><a href="logout.php"><i class="fas fa-cart-arrow-down"></i></a></li>
+      </ul>
+    </header>
+
+
+    <footer>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-6 col-footer">
+            <p>email: unifood@gmail.com</p>
+          </div>
+          <div class="col-sm-6 col-footer">
+            <p>&copy; Elia Pasqualini & Marco Pazzaglia</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+  </body>
+</html>
