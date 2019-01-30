50d3d578bb727548da6f9fbce93ea3f7c8aa7183
Elia <elia.pasqualini@studio.unibo.it>
2019-01-30 16:04:21 +0100

    manca l'aggiunta al carrello

--- ordine.php
+++ ordine.php
@@ -50,8 +50,35 @@ session_start();
 
     <div class="container">
       <div class="row">
-        <div class="col-sm-12">
-          <h1><?php echo $_GET['ristoranteID']; ?></h1>
+        <div class="col-4">
+          <?php
+      		if ($conn->connect_errno) {
+      		?>
+      			<p class="error">Connessione fallita: <?php echo $conn->connect_errno; ?> <?php echo $conn->connect_error; ?></p>
+      		<?php
+      		}
+      		else{
+            $query_sql="SELECT * FROM `ristorante` WHERE idRistorante = '" . $_GET['ristoranteID'] . "'";
+            $result = $conn->query($query_sql);
+            $row = $result->fetch_assoc();
+            $id = $row["idAccount"];
+            if ($row['logo'] == null){
+            ?>
+            <img src='image/food.png' class='mr-5 image-risto' width='70%' height='auto' alt='ristorante'>
+            <?php
+            }
+            else{
+
+             echo '<img width="70%" height="auto" class="mr-5 image-risto" alt="ristorante" src="data:image/jpeg;base64,'.base64_encode( $row['logo'] ).'"/>';
+
+
+             }
+             ?>
+
+
+        </div>
+        <div class="col-8">
+          <h1 class="titlee" valign=""><?php echo $row['nome']; ?> </h1>
         </div>
       </div>
     </div>
@@ -63,6 +90,12 @@ session_start();
 
 
     <div class="card table-responsive">
+      <?php
+      $query_sql="SELECT * FROM `menu` WHERE idRistorante = '" . $_GET['ristoranteID'] . "'";
+      $result = $conn->query($query_sql);
+      if ($result->num_rows > 0){
+
+       ?>
       <table class="table shopping-cart-wrap">
         <thead class="text-muted">
           <tr>
@@ -73,35 +106,58 @@ session_start();
           </tr>
         </thead>
         <tbody>
+          <?php
+            while($row = $result->fetch_assoc()) {
+          ?>
           <tr>
     	       <td>
 
-    		              <h5 class="title text-truncate">Margherita</h5>
+    		              <h5 class="title text-truncate"><?php echo $row["nome"]; ?> </h5>
     		                <dl class="param param-inline small">
     		                    <dt>Categoria: </dt>
-    		                      <dd>Pizza</dd>
+                            <dd><?php echo $row["categoria"]; ?></dd>
     		                </dl>
     		                <dl class="param param-inline small">
-    		                    <dt>Ingredienti: </dt>
-    		                    <dd>Pomodoro, Mozzarella</dd>
+                          <?php
+                          $sql = "SELECT nomeIngrediente FROM `ingrediente` WHERE idPiatto = '" . $row['idPiatto'] . "'";
+                          $result2 = $conn->query($sql);
+                          ?>
+    		                  <dt>Ingredienti: </dt>
+                          <dd>
+                            <?php  while ($righe = $result2->fetch_assoc()){
+
+                                echo $righe['nomeIngrediente'];
+                                echo "  ";
+                              }
+                            ?></dd>
     		                </dl>
 
-    	        </td>
+    	           </td>
     	        <td>
-
-  <input type="number" value="1" class="form-control">
-
+                <input type="number" value="0" class="form-control">
     	        </td>
     	        <td>
             		<div class="price-wrap">
-            			<var class="price">5.5&euro;</var><br>
+            			<var class="price"><?php echo $row["prezzo"]; ?>&euro;</var><br>
             			<small class="text-muted">(&euro; per ogni pz.)</small>
             		</div> <!-- price-wrap .// -->
     	        </td>
     	        <td>
-    	        <a title="" href="" class="btn btn-outline-success" data-toggle="tooltip" data-original-title="Save to Wishlist"> <i class="fas fa-plus-circle"></i></a>
+    	        <button class="btn btn-outline-success text-center"> <i class="fas fa-plus-circle"></i></button>
     	        </td>
             </tr>
+            <?php
+            }
+
+
+          }
+          else {
+          ?>
+            <p>Nessun piatto a disposizione nel menù</p>
+          <?php
+          }
+          ?>
+          <!--prova
             <tr>
       	       <td>
 
@@ -117,16 +173,16 @@ session_start();
 
       	        </td>
       	        <td>
-                    <input type="number" value="1" class="form-control">
+                    <input type="number" value="0" class="form-control">
       	        </td>
       	        <td>
               		<div class="price-wrap">
               			<var class="price">5.5&euro;</var><br>
               			<small class="text-muted">(&euro; per ogni pz.)</small>
-              		</div> <!-- price-wrap .// -->
+              		</div>
       	        </td>
       	        <td>
-      	        <a title="" href="" class="btn btn-outline-success" data-toggle="tooltip" data-original-title="Save to Wishlist"> <i class="fas fa-plus-circle"></i></a>
+      	        <button class="btn btn-outline-success text-center"> <i class="fas fa-plus-circle"></i></button>
       	        </td>
               </tr>
               <tr>
@@ -144,19 +200,20 @@ session_start();
 
                   </td>
                   <td>
-                      <input type="number" value="1" class="form-control">
+                      <input type="number" value="0" class="form-control">
                   </td>
                   <td>
                     <div class="price-wrap">
                       <var class="price">5.5&euro;</var><br>
                       <small class="text-muted">(&euro; per ogni pz.)</small>
-                    </div> <!-- price-wrap .// -->
+                    </div>
                   </td>
                   <td>
                   <button class="btn btn-outline-success text-center"> <i class="fas fa-plus-circle"></i></button>
 
                   </td>
                 </tr>
+                 -->
               </tbody>
             </table>
           </div> <!-- card.// -->
@@ -177,6 +234,17 @@ session_start();
         </div>
       </div>
     </footer>
+
+    <?php
+      //Chiusura connessione con db
+      $conn->close();
+    }
+    ?>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/navbar.js" type="text/javascript"></script>
 
   </body>
 </html>
